0dcf7417b9f3cb522cef75fdd734a1c08e6a685d
Jed Ludlow <jed.ludlow@gmail.com>
2021-12-31 14:01:47 -0700

    Augments unit tests for normal and lognormal.
    Add unit tests for the exact method and for Howe's approximate method.
    
    The exact tests here are just designed to check that
    functions are hooked up correctly
    since there are already exhaustive tests for the exact two-sided normal
    tolerance factors in other separate unit tests.

--- tests/test_twoside_normal.py
+++ tests/test_twoside_normal.py
@@ -7,6 +7,31 @@ import unittest
 
 class TestEverything(unittest.TestCase):
 
+    def _run_test_cases(self, N, P, G, K, method):
+        for i, k in enumerate(K):
+            n = N[i]
+            x = np.random.random(n) * 10
+            xmu = x.mean()
+            xstd = x.std(ddof=1)
+            p = P[i]
+            g = G[i]
+            bound = normal(x, p, g, method=method)
+            k_hat_l = (xmu - bound[0, 0]) / xstd
+            k_hat_u = (bound[0, 1] - xmu) / xstd
+            self.assertTrue(np.isclose(k, k_hat_l, rtol=1e-4, atol=1e-5))
+            self.assertTrue(np.isclose(k, k_hat_u, rtol=1e-4, atol=1e-5))
+
+    def test_exact(self):
+        # We test only one case here mostly as a sanity check to make sure
+        # the functions are hooked up correctly. See two-sided tolerance
+        # factor unit tests for exhaustive checking of exact tolerance factors.
+        # Test case is drawn from ISO Table F1.
+        G = [0.90]
+        N = [35]
+        P = [0.90]
+        K = [1.9906]
+        self._run_test_cases(N, P, G, K, 'exact')
+
     def test_guenther_approx(self):
         # values from:
         # https://ncss-wpengine.netdna-ssl.com/wp-content/themes/ncss/pdf/Procedures/PASS/Tolerance_Intervals_for_Normal_Data.pdf
@@ -16,18 +41,18 @@ class TestEverything(unittest.TestCase):
         P = [0.8, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9]
         K = [1.6124, 1.7984, 1.7493, 1.7287, 1.7168, 1.7088, 1.7029, 1.6984,
              1.6948, 1.6703, 1.7138, 1.8084]
-        for i, k in enumerate(K):
-            n = N[i]
-            x = np.random.random(n) * 10
-            xmu = x.mean()
-            xstd = x.std(ddof=1)
-            p = P[i]
-            g = G[i]
-            bound = normal(x, p, g, method='guenther')
-            k_hat_l = (xmu - bound[0, 0]) / xstd
-            k_hat_u = (bound[0, 1] - xmu) / xstd
-            self.assertTrue(np.isclose(k, k_hat_l, rtol=1e-4, atol=1e-5))
-            self.assertTrue(np.isclose(k, k_hat_u, rtol=1e-4, atol=1e-5))
+        self._run_test_cases(N, P, G, K, 'guenther')
+
+    def test_howe_approx(self):
+        # values from:
+        # https://www.itl.nist.gov/div898/handbook/prc/section2/prc263.htm
+        # Howe's method is implicitly tested to some extent by Guenther's
+        # method.
+        G = [0.99]
+        N = [43]
+        P = [0.9]
+        K = [2.2173]
+        self._run_test_cases(N, P, G, K, 'howe')
 
     def test_random_shapes(self):
         M = [3, 10, 20]
